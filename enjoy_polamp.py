2a49f1b43fe0e22275a57562ac36de7f8738fab4
Gricha1 <trololoeeev@mail.ru>
2022-07-11 02:36:15 +0300

    update server info

--- enjoy_polamp.py
+++ enjoy_polamp.py
@@ -20,7 +20,7 @@ from sample_factory.envs.create_env import create_env
 from sample_factory.utils.utils import log, AttrDict
 
 
-def enjoy(init_cfg, max_num_frames=450, use_wandb=True):
+def enjoy(init_cfg, max_num_frames=900, use_wandb=True):
     save_image = True
     save_obs = False
     done_save_img = False
